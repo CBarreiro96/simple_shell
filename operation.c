2c4399eb698e46288c568cac86f223665bac927c
Esteban <esteban.tvop@gmail.com>
2020-08-10 11:18:42 -0500

    operation init

---
+++ operation.c
@@ -0,0 +1,25 @@
+#include "holberton.c"
+/**
+ *
+ *
+ *
+ *
+ */
+void simple_print(void)
+{
+	write(STDOUT_FILENO, "$ ",2);
+
+}
+/**
+ * _strlen_const - strlen for const strings
+ * @s: string to be measured
+ * Return: length of string
+ */
+int _strlen_const(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
